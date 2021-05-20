e7b54f238b9d697f64f23a0eb7186976e8ff1e7b
mihai67 <mihai67@gmail.com>
2021-05-20 17:55:56 +0000

    Added vpc module 3

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "app.terraform.io/mihail67-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name ="mihai-network"
+  network_name ="testi-net"
   project_id = var.project
   subnets = [
   {
-    subnet_name   = "mihai-subnet"
+    subnet_name   = "gram-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
